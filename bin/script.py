31530d6c4d59ed093e8971cc3b0099253ba14f8a
Akhil <singhakhil33@gmail.com>
2015-08-31 04:23:12 +0530

    script added and changes to VM_INFO

---
+++ bin/script.py
@@ -0,0 +1,8 @@
+import os
+import sys
+if __name__ == '__main__':
+	if len(sys.argv) < 4:
+			print "Format: ./script pm_file image_file flavor_file"
+			exit(1)
+	os.chdir("../src")
+	os.system("sudo python main.py " + sys.argv[1] + " " +sys.argv[2] + " " +sys.argv[3])
